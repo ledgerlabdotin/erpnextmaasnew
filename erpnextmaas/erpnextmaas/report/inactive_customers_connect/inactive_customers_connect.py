06e44a389af56d1573e04dadf5523441dc101668
hamzaali15 <malikhamzaali48@gmail.com>
2024-06-09 13:44:09 +0500

    cust: Inactive Customers Connect report updated

--- erpnextmaas/erpnextmaas/report/inactive_customers_connect/inactive_customers_connect.py
+++ erpnextmaas/erpnextmaas/report/inactive_customers_connect/inactive_customers_connect.py
@@ -21,7 +21,9 @@ def execute(filters=None):
 
 	data = []
 	for cust in customers_connects_list:
-		if cust.days_since_last_connect >= cint(days_since_last_connect):
+		if cust.days_since_last_connect and cust.days_since_last_connect >= cint(days_since_last_connect):
+			data.append(cust)
+		elif not cust.days_since_last_connect:
 			data.append(cust)
 	return columns, data
 
@@ -71,13 +73,15 @@ def get_columns():
 
 
 def get_data():
-	return frappe.db.sql("""select
-			date, time, customer_name,
-			connect_type, value_created, remarks,
-			count(distinct(name)) as num_of_connect,
-			max(date) as last_connect_date,
-			DATEDIFF(CURRENT_DATE, max(date)) as days_since_last_connect
-		from `tabCustomer Connect`
-		group by customer_name
-		order by days_since_last_connect desc
-	""", as_dict=True)
+	query = """select
+				cc.date, cc.time, cust.name, cust.customer_name,
+				cc.connect_type, cc.value_created, cc.remarks,
+				count(distinct(cc.name)) as num_of_connect,
+				max(cc.date) as last_connect_date,
+				DATEDIFF(CURRENT_DATE, max(cc.date)) as days_since_last_connect
+			from `tabCustomer` as cust
+			left join `tabCustomer Connect` as cc on cc.customer_name=cust.name
+			group by cust.name
+			order by days_since_last_connect desc
+		"""
+	return frappe.db.sql(query, as_dict=True)
